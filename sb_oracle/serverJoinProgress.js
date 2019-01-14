b1116b3257828a273ea63e75002d979ec7204de9
123 Notus <45607902+123-Notus@users.noreply.github.com>
2019-01-14 00:23:28 +0000

    Update serverJoinProgress.js

--- sb_oracle/serverJoinProgress.js
+++ sb_oracle/serverJoinProgress.js
@@ -1,4 +1,4 @@
-    jQuery.getJSON('http://starblast.io/simstatus.json', function (data) {  
+    jQuery.getJSON('https://starblast.io/simstatus.json', function (data) {  
         var socket = new eio.Socket('45.32.193.42:3060');
       socket.on('open', function(){
         socket.send("ping");
